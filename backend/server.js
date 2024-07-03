c19e204ace4d90c84020c35e8c5286a8b0b484f1
KhunKyawTunwin <kyawtunwin000782@gmail.com>
2024-07-03 14:46:22 +0700

    config _env_backend

--- backend/server.js
+++ backend/server.js
@@ -21,20 +21,17 @@ connectDatabase();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-})
-
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // create server
 const server = app.listen(process.env.PORT, () => {
-  console.log(
-    `Server is running on http://localhost:${process.env.PORT}`
-  );
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
 
 // unhandled promise rejection
 process.on("unhandledRejection", (err) => {
-  console.log(`Shutting down the server for ${err.message}`);
+  console.log(`Shutting down the server for : ${err.message}`);
   console.log(`shutting down the server for unhandle promise rejection`);
 
   server.close(() => {
